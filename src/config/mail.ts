818a54e86cdeef7c4e4eef85d326d6955ba36059
nebiyu <nebiyuf@gmail.com>
2023-06-10 13:05:03 +0300

    mail added

---
+++ src/config/mail.ts
@@ -0,0 +1,33 @@
+import nodemailer from "nodemailer";
+
+export async function sendMail(
+  to: string,
+  body: {
+    name: string;
+    email: string;
+    phone: string;
+    message: string;
+  }
+) {
+  let transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL, // generated ethereal user
+      pass: process.env.EMAIL_PASSWORD, //
+    },
+  });
+  await transporter.sendMail({
+    from: process.env.EMAIL, // sender address
+    to, // list of receivers
+    subject: "Contact information", // Subject line
+    text: "Hello world?", // plain text body
+    html: `
+    <b>Name:</b> ${body.name} <br/>
+    <b>Email:</b> ${body.email} <br/>
+    <b>Phone:</b> ${body.phone} <br/>
+    <b>Message:</b> ${body.message} <br/>
+`, // html body
+  });
+}
