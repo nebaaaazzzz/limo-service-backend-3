d1f787576750477ef3c7cc56ddf5018365b065d1
Nebiyu <60134585+nebaaaazzzz@users.noreply.github.com>
2023-06-21 16:37:52 +0300

    Update vehicle.controller.ts

--- src/controller/vehicle.controller.ts
+++ src/controller/vehicle.controller.ts
@@ -44,7 +44,7 @@ export const postVehicle = [
 export const getVehicles = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const page = Number(req.query?.page) || 1;
-    const PAGE_SIZE = 3;
+    const PAGE_SIZE = 6;
     const limit = Number(req.query?.limit) || PAGE_SIZE;
     const results = await Vehicle.findMany({
       skip: (page - 1) * limit,
