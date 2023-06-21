8c21ba451d2143895b6de4bc1961c14a388103d7
Nebiyu <60134585+nebaaaazzzz@users.noreply.github.com>
2023-06-21 16:27:35 +0300

    Update vehicle.controller.js

--- build/controller/vehicle.controller.js
+++ build/controller/vehicle.controller.js
@@ -68,7 +68,7 @@ const postVehicle = [
 ];
 const getVehicles = (0, _error.catchAsync)(async (req, res, next)=>{
     const page = Number(req.query?.page) || 1;
-    const PAGE_SIZE = 6;
+    const PAGE_SIZE = 3;
     const limit = Number(req.query?.limit) || PAGE_SIZE;
     const results = await _db.Vehicle.findMany({
         skip: (page - 1) * limit,
